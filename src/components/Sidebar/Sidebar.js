276cb50d04ee7773dd9a150d54812d771070ba50
Syméon Smith <symeon.smith@gmail.com>
2021-10-03 22:30:55 +0200

    Add style to sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -21,17 +21,20 @@ class Sidebar extends React.Component {
 
     return (
       <div className="Sidebar">
-        <img src={"https://openweathermap.org/img/wn/" + weather.weather[0].icon + "@4x.png"} alt="" />
+        <img className="currentWeatherIcon" src={"https://openweathermap.org/img/wn/" + weather.weather[0].icon + "@4x.png"} alt={weatherDescription} />
         <h1>{temp}°
         {
         temp !== feelsLike
-          ? (<span className="feelsLike">(Feels like {feelsLike})</span>)
+              ? (<span className="feelsLike">Feels like {feelsLike}°</span>)
           : ""
         }
         </h1>
         <h2>{weekDay}, <span className="hour">{hours}:{minutes}</span></h2>
         <hr />
         <p className="weatherDescription">{weatherDescription}</p>
+        <div className="cityBox">
+          <p>{weather.name}, {weather.sys.country}</p>
+        </div>
       </div>
     );
   }
