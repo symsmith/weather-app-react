2c20a0991ffe93eff7a068f326909707a5d89012
Syméon Smith <symeon.smith@gmail.com>
2021-10-25 18:43:19 +0200

    Convert Sidebar to functional component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,107 +1,104 @@
-import React from 'react';
-import './Sidebar.css';
-import { getName, registerLocale } from 'i18n-iso-countries';
-registerLocale(require("i18n-iso-countries/langs/en.json"));
+import React, { useState } from "react"
+import "./Sidebar.css"
+import { getName, registerLocale } from "i18n-iso-countries"
+registerLocale(require("i18n-iso-countries/langs/en.json"))
 
+const Sidebar = ({ onSearch, currentWeather }) => {
+  const [cityName, setCityName] = useState("")
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { cityName: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const handleChange = (event) => {
+    setCityName(event.target.value)
   }
 
-  handleChange(event) {
-    this.setState({ cityName: event.target.value });
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSearch(cityName)
+    setCityName("")
+    /* Remove focus on input */
+    event.target[0].blur()
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onSearch(this.state.cityName);
-    this.setState({ cityName: "" });
-    event.target[0].blur();
-  }
+  const temp = Math.round(currentWeather.main.temp)
+  const feelsLike = Math.round(currentWeather.main.feels_like)
 
-  render() {
-    /* Handle error in request */
-    if (this.props.currentWeather.cod && this.props.currentWeather.cod !== 200) {
-      return (
-        <div className="Sidebar">
-          <form onSubmit={this.handleSubmit} className="searchForm">
-            <div className="iconInput">
-              <span className="searchIcon">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-5 h-5"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-              </span>
-              <input
-                type="text"
-                className="countrySearch"
-                placeholder="City not found"
-                value={this.state.cityName}
-                onChange={this.handleChange}
-              />
-            </div>
-            <input type="submit" className="searchButton" onSubmit={this.handleSubmit} />
-          </form>
-        </div>
-      )
-    }
-    else {
-      let weather = this.props.currentWeather;
-      let temp = Math.round(weather.main.temp);
-      let feelsLike = Math.round(weather.main.feels_like);
-      let date = new Date()
-      let weekDay = date.toLocaleString("en-US", { weekday: "long" });
-      let hours = date.getHours().toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-      });
-      let minutes = date.getMinutes().toLocaleString('en-US', {
-        minimumIntegerDigits: 2,
-        useGrouping: false
-      });
-      let weatherDescription = weather.weather[0].description.charAt(0).toUpperCase()
-        + weather.weather[0].description.slice(1);
+  /* Get local time and weekday */
+  const date = new Date()
+  const weekDay = date.toLocaleString("en-US", { weekday: "long" })
+  const hours = date.getHours().toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  })
+  const minutes = date.getMinutes().toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  })
 
-      return (
-        <div className="Sidebar">
-          <form onSubmit={this.handleSubmit} className="searchForm">
-            <div className="iconInput">
-              <span className="searchIcon">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-5 h-5"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-              </span>
-              <input
-                type="text"
-                className="countrySearch"
-                placeholder={weather.name + ", " + getName(weather.sys.country, "en", { select: "alias" })}
-                value={this.state.cityName}
-                onChange={this.handleChange}
-              />
-            </div>
-            <input type="submit" className="searchButton" onSubmit={this.handleSubmit} />
-          </form>
-          <img
-            className="currentWeatherIcon"
-            src={"https://openweathermap.org/img/wn/" + weather.weather[0].icon + "@4x.png"}
-            alt={weatherDescription}
+  /* Capitalize the first letter */
+  const weatherDescription =
+    currentWeather.weather[0].description.charAt(0).toUpperCase() +
+    currentWeather.weather[0].description.slice(1)
+
+  return (
+    <div className="Sidebar">
+      <form onSubmit={handleSubmit} className="searchForm">
+        <div className="iconInput">
+          <span className="searchIcon">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              className="w-5 h-5"
+            >
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+          </span>
+          <input
+            type="text"
+            className="countrySearch"
+            placeholder={
+              currentWeather.name +
+              ", " +
+              getName(currentWeather.sys.country, "en", { select: "alias" })
+            }
+            value={cityName}
+            onChange={handleChange}
           />
-          <h1>{temp}°
-          {
-          temp !== feelsLike
-                ? (<span className="feelsLike">Feels like <span className="font-serif">{feelsLike}°</span></span>)
-            : ""
-          }
-          </h1>
-          <div className="alignment">
-            <h2>{weekDay}, <span className="hour">{hours}:{minutes}</span></h2>
-            <hr />
-            <p className="weatherDescription">{weatherDescription}</p>
-          </div>
         </div>
-      );
-    }
-  }
+      </form>
+      <img
+        className="currentWeatherIcon"
+        src={
+          "https://openweathermap.org/img/wn/" +
+          currentWeather.weather[0].icon +
+          "@4x.png"
+        }
+        alt={weatherDescription}
+      />
+      <h1>
+        {temp}°
+        {temp !== feelsLike ? (
+          <span className="feelsLike">
+            Feels like <span className="font-serif">{feelsLike}°</span>
+          </span>
+        ) : (
+          ""
+        )}
+      </h1>
+      <div className="alignment">
+        <h2>
+          {weekDay},{" "}
+          <span className="hour">
+            {hours}:{minutes}
+          </span>
+        </h2>
+        <hr />
+        <p className="weatherDescription">{weatherDescription}</p>
+      </div>
+    </div>
+  )
 }
 
-export default Sidebar;
+export default Sidebar
