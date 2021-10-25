81ee9375b330003766391c0d51be45cf5a36f1ba
Syméon Smith <symeon.smith@gmail.com>
2021-10-25 19:32:27 +0200

    Convert DetailList to functional component

--- src/components/Content/DetailList/DetailList.js
+++ src/components/Content/DetailList/DetailList.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import Day from './Detail/Day';
-import Hour from './Detail/Hour';
-import './DetailList.css';
+import React from "react"
+import Day from "./Detail/Day"
+import Hour from "./Detail/Hour"
+import "./DetailList.css"
 
-class DetailList extends React.Component {
-  render() {
-    const daily = this.props.type === "daily";
-    const days = Array.from(Array(7), (e, i) => i + 1);
-    const hours = Array.from(Array(24), (e, i) => i + 1);
-    return (
-      <div className={"DetailList" + (daily ? " DailyList" : " HourlyList")}>
-        {daily ? days.map((day) =>
-          <Day dailyForecast={this.props.forecast[day]} dayShift={day} key={day.toString()} />) :
-          hours.map((hour) =>
-            <Hour hourlyForecast={this.props.forecast[hour]} hourShift={hour} key={hour.toString()} />
-          )}
-      </div>
-    );
-  }
+const DetailList = ({ type, forecast }) => {
+  const daily = type === "daily"
+  const days = Array.from(Array(7), (e, i) => i + 1)
+  const hours = Array.from(Array(24), (e, i) => i + 1)
+
+  return (
+    <div className={"DetailList" + (daily ? " DailyList" : " HourlyList")}>
+      {daily
+        ? days.map((day) => (
+            <Day
+              dailyForecast={forecast[day]}
+              dayShift={day}
+              key={day.toString()}
+            />
+          ))
+        : hours.map((hour) => (
+            <Hour
+              hourlyForecast={forecast[hour]}
+              hourShift={hour}
+              key={hour.toString()}
+            />
+          ))}
+    </div>
+  )
 }
 
-
-export default DetailList;
+export default DetailList
