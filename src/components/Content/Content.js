4fd3e4fc071ca8cdabdd3e4decde81fa950bb323
Syméon Smith <symeon.smith@gmail.com>
2021-10-25 19:28:36 +0200

    Convert Content to functional component

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,119 +1,84 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import apiUtils from "../../utils/apiCalls"
 import DetailList from "./DetailList/DetailList"
 import Highlights from "./Highlights/Highlights"
 import "./Content.css"
 
-class Content extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      error: null,
-      isLoaded: false,
-      oldCoord: this.props.currentWeather.coord,
-      dailyForecast: {},
-      hourlyForecast: {},
-      weekActive: false
-    }
-  }
+const Content = ({ currentWeather, unitSystem }) => {
+  const [detailedForecast, setDetailedForecast] = useState({})
+  const [dailyActive, setDailyActive] = useState(false)
 
-  fetchDetails() {
+  const fetchDetails = () => {
     apiUtils
       .fetchForecast(
         {
-          lat: this.props.currentWeather.coord.lat,
-          lon: this.props.currentWeather.coord.lon
+          lat: currentWeather.coord.lat,
+          lon: currentWeather.coord.lon
         },
-        this.props.unitSystem
+        unitSystem
       )
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            dailyForecast: result.daily,
-            hourlyForecast: result.hourly,
-            oldCoord: { lat: result.lat, lon: result.lon }
-          })
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
-        }
-      )
+      .then((result) => {
+        setDetailedForecast(result)
+      })
+      .catch((e) => {
+        console.log("Could not fetch details")
+      })
   }
 
-  componentDidMount() {
-    this.fetchDetails()
-  }
+  /* Fetch forecast details on component mount */
+  useEffect(() => {
+    fetchDetails()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-  componentDidUpdate() {
-    if (
-      this.props.currentWeather.coord.lat !== this.state.oldCoord.lat ||
-      this.props.currentWeather.coord.lon !== this.state.oldCoord.lon
-    ) {
-      this.fetchDetails()
-    }
-  }
+  /* Fetch forecast details when coordinates in props change */
+  useEffect(() => {
+    fetchDetails()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentWeather.coord])
 
-  render() {
-    if (
-      this.props.currentWeather.cod &&
-      this.props.currentWeather.cod !== 200
-    ) {
-      return <div className="Content"></div>
-    } else {
-      const { error, isLoaded, dailyForecast, hourlyForecast, weekActive } =
-        this.state
-      if (error) {
-        return <div>Error: {error.message}</div>
-      } else if (!isLoaded) {
-        return <div>Loading…</div>
-      } else {
-        return (
-          <div className="Content">
-            <h3 className="cardsSelect">
-              <span
-                className={
-                  "timespanButton" +
-                  (!weekActive ? " timespanButtonActive" : "")
-                }
-                onClick={() => this.setState({ weekActive: false })}
-              >
-                Today
-              </span>
-              <span
-                className={
-                  "timespanButton" + (weekActive ? " timespanButtonActive" : "")
-                }
-                onClick={() => this.setState({ weekActive: true })}
-              >
-                Week
-              </span>
-            </h3>
-            {weekActive ? (
-              <DetailList
-                type="daily"
-                forecast={dailyForecast}
-                unitSystem={this.props.unitSystem}
-              />
-            ) : (
-              <DetailList
-                type="hourly"
-                forecast={hourlyForecast}
-                unitSystem={this.props.unitSystem}
-              />
-            )}
-            <h2 className="highlightsTitle">Today's Highlights</h2>
-            <Highlights
-              weather={this.props.currentWeather}
-              unitSystem={this.props.unitSystem}
-            />
-          </div>
-        )
-      }
-    }
+  if (Object.keys(detailedForecast).length === 0) {
+    return <div>Loading…</div>
+  } else {
+    const hourlyForecast = detailedForecast.hourly
+    const dailyForecast = detailedForecast.daily
+    return (
+      <div className="Content">
+        <h3 className="cardsSelect">
+          <span
+            className={
+              "timespanButton" + (!dailyActive ? " timespanButtonActive" : "")
+            }
+            onClick={() => setDailyActive(false)}
+          >
+            Today
+          </span>
+          <span
+            className={
+              "timespanButton" + (dailyActive ? " timespanButtonActive" : "")
+            }
+            onClick={() => setDailyActive(true)}
+          >
+            Week
+          </span>
+        </h3>
+        {dailyActive ? (
+          <DetailList
+            type="daily"
+            forecast={dailyForecast}
+            unitSystem={unitSystem}
+          />
+        ) : (
+          <DetailList
+            type="hourly"
+            forecast={hourlyForecast}
+            unitSystem={unitSystem}
+          />
+        )}
+        <h2 className="highlightsTitle">Today's Highlights</h2>
+        <Highlights weather={currentWeather} unitSystem={unitSystem} />
+      </div>
+    )
   }
 }
 
